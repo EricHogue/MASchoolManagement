1fbbc0c6a1670a984008eb9f3a9dd8e58a04af48
Eric Hogue <eric@erichogue.ca>
2011-12-22 10:05:20 -0500

    Skip test, will implement later

---
+++ tests/unittests/MASchoolManagement/Subscriptions/UserSubscriptionsTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace MASchoolManagement\Subscriptions;
+
+class UserSubscriptionsTest extends \PHPUnit_Framework_TestCase {
+	public function setup() {
+	}
+
+	public function testTest() {
+		$this->assertTrue(true);
+	}
+}
